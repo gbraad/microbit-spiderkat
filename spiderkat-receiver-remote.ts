aec2e573e8848cf4b0661c027aac419fe69e2951
Gerard Braad <me@gbraad.nl>
2019-11-16 00:56:49 +0800

    Untested motor protocol

--- spiderkat-receiver-remote.ts
+++ spiderkat-receiver-remote.ts
@@ -1,40 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(0)
-})
-
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        255,
-        robotbit.Motors.M1B,
-        255
-        )
-    }
-    if (receivedNumber == 1) {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        255,
-        robotbit.Motors.M1B,
-        0
-        )
-    }
-    if (receivedNumber == 2) {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        0,
-        robotbit.Motors.M1B,
-        255
-        )
-    }
-})
-
 radio.setGroup(1)
 basic.showLeds(`
     . . . . .
@@ -45,5 +8,31 @@ basic.showLeds(`
     `)
 
 basic.forever(function () {
-	
+    // QnD
+    radio.sendString("M 0 0")
+})
+
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("M -255 255")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("M 255 -255")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("M 255 255")
+})
+
+radio.onReceivedString(function (receivedString) {
+    let elements = receivedString.split(" ")
+    if (elements[0] == "M") {
+        let m1aValue = parseInt(elements[1])
+        let m1bValue = parseInt(elements[2])
+
+        robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            m1aValue,
+            robotbit.Motors.M1B,
+            m1bValue
+        )
+    }
 })
